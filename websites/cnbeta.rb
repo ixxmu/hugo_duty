6022941f8b311f43a21d97b8eaa139ac29761383
ixxmu <ixxmu@users.noreply.github.com>
2023-05-03 20:46:17 +0800

    Update cnbeta.rb

--- websites/cnbeta.rb
+++ websites/cnbeta.rb
@@ -5,7 +5,7 @@ register_website(
   },
   request: -> (uri) {
     headers = {
-      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:74.0) Gecko/20100101 Firefox/74.0'
+      'User-Agent' => 'Mozilla/5.0 (Android 13; Mobile; rv:109) Gecko/112.0 Firefox/112.0'
     }
 
     get_with_headers(uri, headers)
