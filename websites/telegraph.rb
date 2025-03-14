fe142e7ffca045a2cb39ccb856b41f2026798b86
ixxmu <ixxmu@users.noreply.github.com>
2025-03-14 08:44:25 +0800

    Update telegraph.rb

--- websites/telegraph.rb
+++ websites/telegraph.rb
@@ -6,9 +6,9 @@ register_website(
   },
   process: -> (html) {
     document = Nokogiri::HTML(html)
-    title = document.css('header h1').first.content
-    author = document.css('header a[rel=author]').first.content
-    content = document.css('article').first
+    title = document.css('#activity-name').first&.content&.strip || '标题未找到'
+    author = document.css('#js_name').first&.content&.strip || '作者未找到'
+    content = document.css('#js_content').first || raise("无法找到正文")
 
     content.css('h1').first.remove
     content.css('address').first.remove
